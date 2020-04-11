5e3ea8f7011e4d7f3c34e50f8fbbe10094d37e01
elenaslavutina <elena.slavutina2013@gmail.com>
2020-04-11 17:20:27 +0300

    newtest is added

--- src/test/java/ru/netology/statistic/StaticServiceTest.java
+++ src/test/java/ru/netology/statistic/StaticServiceTest.java
@@ -7,11 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class StaticServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxWhenItFirst() {
         StaticService service = new StaticService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+@Test
+    void findMaxWhenItLast() {
+        StaticService service = new StaticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
