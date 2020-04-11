44283f24bff28bbad1f87d76ae9064d4fa356c57
elenaslavutina <elena.slavutina2013@gmail.com>
2020-04-11 18:13:07 +0300

    newtest is hide

--- src/test/java/ru/netology/statistic/StaticServiceTest.java
+++ src/test/java/ru/netology/statistic/StaticServiceTest.java
@@ -10,22 +10,23 @@ class StaticServiceTest {
     void findMaxWhenItFirst() {
         StaticService service = new StaticService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
-@Test
-    void findMaxWhenItLast() {
-        StaticService service = new StaticService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long[] incomesInBillions = {13, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+
+  //  @Test
+  //  void findMaxWhenItLast() {
+  //      StaticService service = new StaticService();
+
+  //      long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+  //      long expected = 13;
+
+ //       long actual = service.findMax(incomesInBillions);
+
+   //     assertEquals(expected, actual);
+   // }
 }
