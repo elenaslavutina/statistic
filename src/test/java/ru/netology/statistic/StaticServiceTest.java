18655e052df09a9a9466185cf4ac9173e8bf95a9
elenaslavutina <elena.slavutina2013@gmail.com>
2020-04-11 18:16:44 +0300

    newtest is update

--- src/test/java/ru/netology/statistic/StaticServiceTest.java
+++ src/test/java/ru/netology/statistic/StaticServiceTest.java
@@ -18,15 +18,15 @@ class StaticServiceTest {
         assertEquals(expected, actual);
     }
 
-  //  @Test
-  //  void findMaxWhenItLast() {
-  //      StaticService service = new StaticService();
+    @Test
+    void findMaxWhenItLast() {
+        StaticService service = new StaticService();
 
-  //      long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
-  //      long expected = 13;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
 
- //       long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-   //     assertEquals(expected, actual);
-   // }
+      assertEquals(expected, actual);
+  }
 }
